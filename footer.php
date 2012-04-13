14203f0072e29d2fc038cc0c34ed021932ab7cb4
Alexander Agnarson <alexander.agnarson@gmail.com>
2012-04-13 20:57:05 +0200

    removed footer-widgets section

--- footer.php
+++ footer.php
@@ -7,15 +7,6 @@
 		</div><!--/subfooter-inner-->
 	</div><!--/subfooter-->
 	
-	<div id="footer-widgets">
-		<div id="footer-widgets-inner" class="container-24">
-		
-			<p>test</p>
-			
-			<div class="clear"></div>
-		</div><!--/footer-widgets-inner-->
-	</div><!--/footer-widgets-->
-	
 	<footer id="footer">
 		<div id="footer-inner" class="container-24">
 		
