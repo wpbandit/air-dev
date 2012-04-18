691ab9725399f8f293351083171376505b106df0
Jermaine Maree <jermaine@banditmedia.com>
2012-04-18 11:29:04 -0400

    form and icon boxes

---
+++ air/modules/social/social-options-page.php
@@ -0,0 +1,30 @@
+<div id="bandit-social" class="air-module-container">
+
+<div class="air-form-box">
+	<h3>Create New Item</h3>
+
+	<p>
+		<label for="social-item-url"><span>URL</span></label>
+		<input id="social-item-url" name="bandit_social[url]" type="text" class="large-text" value="http://">
+	</p>
+			
+	<p>
+		<label for="social-item-name"><span>Label</span></label>
+		<input id="social-item-name" name="bandit_social[name]" type="text" class="large-text">
+	</p>
+
+	<p>
+		<label for="custom-menu-item"><span>Icon URL</span></label>
+		<input id="social-item-icon" name="bandit_social[icon]" type="text" class="large-text">
+	</p>
+
+	<input type="submit" id="bandit_social_submit" class="button-secondary" value="Add Item">
+</div>
+
+<div class="air-icons-box">
+	<h3>Icons</h3>
+
+	<p>icons go here</p>
+</div>
+
+</div><!-- end .air-module-container -->
