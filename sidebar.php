e63d1a0b37d3a9cbf97c93e5d1edb9485d8cf708
Alexander Agnarson <alexander.agnarson@gmail.com>
2012-04-19 16:34:06 +0200

    sidebar areas

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,23 @@
 <ul id="sidebar">
 	<?php if (is_home()) { ?>
-		<?php dynamic_sidebar('sidebar-main'); ?>
-	<?php } elseif (is_page()) { ?>
-		<?php dynamic_sidebar('sidebar-page'); ?>
+		<?php dynamic_sidebar('sidebar'); ?>
+		
 	<?php } elseif (is_single()) { ?>
 		<?php dynamic_sidebar('sidebar-single'); ?>
+		
+	<?php } elseif (is_archive()) { ?>
+		<?php dynamic_sidebar('sidebar-archive'); ?>
+		
+	<?php } elseif (is_page()) { ?>
+		<?php dynamic_sidebar('sidebar-page'); ?>
+		
+	<?php } elseif (is_search()) { ?>
+		<?php dynamic_sidebar('sidebar-search'); ?>
+		
+	<?php } elseif (is_404()) { ?>
+		<?php dynamic_sidebar('sidebar-404'); ?>
+
 	<?php } else { ?>
-		<?php dynamic_sidebar('sidebar-main'); ?>
+		<?php dynamic_sidebar('sidebar'); ?>
 	<?php } ?>
 </ul>
