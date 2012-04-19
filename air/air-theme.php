53d0905f5f8a5b32bbe17243b09d8cd7aac9daab
Jermaine Maree <jermaine@banditmedia.com>
2012-04-19 19:59:47 -0400

    air theme library

---
+++ air/air-theme.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+	Theme Library :: Air Framework
+
+	The contents of this file are subject to the terms of the GNU General
+	Public License Version 2.0. You may not use this file except in
+	compliance with the license. Any of the license terms and conditions
+	can be waived if you get permission from the copyright holder.
+
+	Copyright (c) 2012 WPBandit
+	Jermaine Maree
+
+		@package AirTheme
+		@version 0.1
+**/
+
+//! AirTheme
+class AirTheme extends AirBase {
+
+	/**
+		Initialize
+			@public
+	**/
+	static function init() {
+
+	}
+
+}
