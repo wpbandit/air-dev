9872145a063e6dd8ad601990627dc3061f3a5fb7
Jermaine Maree <jermaine@banditmedia.com>
2012-04-18 20:31:05 -0400

    update filters

--- air/inc/air-filters.php
+++ air/inc/air-filters.php
@@ -5,9 +5,40 @@
 /*-------------------------------------------------------------------------- */
 
 /**
+	Feed Link
+**/
+function air_filter_feedlink($output,$feed) {
+	$feed_url = Air::get_option('feed-url');
+	$feed_array = array(
+		'rss'			=> $feed_url,
+		'rss2'			=> $feed_url,
+		'atom'			=> $feed_url,
+		'rdf'			=> $feed_url,
+		'comments_rss2'	=> ''
+	);
+	$feed_array[$feed] = $feed_url;
+	return $feed_array[$feed];
+}
+if(Air::get_option('feed-url')) {
+	add_filter('feed_link','air_filter_feedlink',10,2);
+}
+
+/**
 	Excerpt More
 **/
 function air_excerpt_more_filter($more) {
 	return '...';
 }
-add_filter('excerpt_more','air_excerpt_more_filter');
+if(Air::get_option('excerpt-more')) {
+	add_filter('excerpt_more','air_excerpt_more_filter');
+}
+
+/**
+	Excerpt Length
+**/
+function air_excerpt_length($length) {
+	return Air::get_option('excerpt-length');
+}
+if(Air::get_option('excerpt-length')) {
+	add_filter( 'excerpt_length','air_excerpt_length',999);
+}
